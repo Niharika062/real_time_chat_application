a7a58e53eb232ddd5160e0851c1126ca41fa84fc
Niharika062 <niharikadogra1318@gmail.com>
2026-06-14 13:24:53 +0530

    online/offline issue fixed

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -12,7 +12,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ['http://localhost:5173', 'https://real-time-chat-application-brown-tau.vercel.app'],
     credentials: true,
   },
 });
